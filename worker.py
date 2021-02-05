886b0a74f2602c79f653e7a9f0d887527d38b561
Young Woo Jung <youngwjung@gmail.com>
2021-02-05 23:18:29 +0900

    fix boto3 region error

--- worker.py
+++ worker.py
@@ -1,12 +1,15 @@
 import boto3
 import random
 import time
+import requests
 
-ssm = boto3.client('ssm')
+region = requests.get('http://169.254.169.254/latest/meta-data/placement/region').text
+
+ssm = boto3.client('ssm', region_name=region)
 resp = ssm.get_parameter(Name='week2_sqs_url')
 SQS_URL=resp['Parameter']['Value']
 
-sqs = boto3.resource('sqs')
+sqs = boto3.resource('sqs', region_name=region)
 queue = sqs.Queue(SQS_URL)
 
 while True:
